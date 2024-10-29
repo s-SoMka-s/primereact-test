ce7829ef5ab8f7f648dc0979122009c0ce3e34c8
Игорь Чиёсов <ichiesov@mac-ichiesov.local>
2024-10-30 00:08:42 +0700

    Init.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import { Button } from 'primereact/button';
+
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+
+function App() {
+  return (
+    <div className="card flex justify-content-center">
+    <Button label="Submit" />
+  </div>
+  );
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Tailwind from 'primereact/passthrough/tailwind';
+
+
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+        
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
+<PrimeReactProvider  >
+      <React.StrictMode>
+      <App />
+      
+
   </React.StrictMode>
+  </PrimeReactProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
